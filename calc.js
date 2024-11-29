3fd2e788edcc4d8f8c0f8377384b9b604787e2d0
Chiemezie <uzohochiemezie@gmail.com>
2024-11-29 22:39:23 +0100

    completed the calculator

---
+++ calc.js
@@ -0,0 +1,89 @@
+const numberBtns = document.querySelectorAll(".number-btn");
+const inputDisplay = document.querySelector(".input-display");
+const operatorBtns = document.querySelectorAll(".operator-btn");
+const equalsBtn = document.querySelector(".equals-btn");
+const allClear = document.querySelector(".ac-btn");
+const delBtn = document.querySelector(".del-btn");
+const maxDigits = 15;
+function add(num1, num2) {
+  return Number(num1) + Number(num2);
+}
+function subtract(num1, num2) {
+  return Number(num1) - Number(num2);
+}
+function multiply(num1, num2) {
+  return Number(num1) * Number(num2);
+}
+function divide(num1, num2) {
+  return Number(num1) / Number(num2);
+}
+let operand1 = "";
+let operand2 = "";
+let operator = "";
+numberBtns.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+   
+    value = event.target.textContent;
+    if (inputDisplay.textContent.includes(".") && value == ".") return;
+    else if (inputDisplay.textContent.length < maxDigits)
+      inputDisplay.textContent += value;
+  });
+});
+
+operatorBtns.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    value = event.target.textContent;
+    operand1 = inputDisplay.textContent;
+    operator = value;
+    inputDisplay.textContent = "";
+  });
+});
+
+equalsBtn.addEventListener("click", () => {
+  operand2 = inputDisplay.textContent;
+  switch (operator) {
+    case "+":
+      inputDisplay.textContent = add(operand1, operand2);
+      if (inputDisplay.textContent.length >= maxDigits) {
+        inputDisplay.textContent = Number(inputDisplay.textContent).toPrecision(
+          maxDigits - 5
+        );
+      }
+      break;
+    case "-":
+      inputDisplay.textContent = subtract(operand1, operand2);
+      if (inputDisplay.textContent.length >= maxDigits) {
+        inputDisplay.textContent = Number(inputDisplay.textContent).toPrecision(
+          maxDigits - 5
+        );
+      }
+      break;
+    case "*":
+      inputDisplay.textContent = multiply(operand1, operand2);
+      if (inputDisplay.textContent.length >= maxDigits) {
+        inputDisplay.textContent = Number(inputDisplay.textContent).toPrecision(
+          maxDigits - 5
+        );
+      }
+      break;
+    case "/":
+      inputDisplay.textContent = divide(operand1, operand2);
+      if (inputDisplay.textContent.length >= maxDigits) {
+        inputDisplay.textContent = Number(inputDisplay.textContent).toPrecision(
+          maxDigits - 2
+        );
+      }
+      break;
+  }
+});
+
+allClear.addEventListener("click", () => {
+  inputDisplay.textContent = "";
+  operand1 = "";
+  operand2 = "";
+  operator = "";
+});
+
+delBtn.addEventListener("click", () => {
+  inputDisplay.textContent = inputDisplay.textContent.slice(0, -1);
+});
